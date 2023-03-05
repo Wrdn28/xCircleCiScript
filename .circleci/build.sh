d537d6cc7f80a40e217c6de3e85a0eb359b80690
Leonard <86230878+Wrdn28@users.noreply.github.com>
2023-03-05 22:10:25 +0700

    bismillah

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,6 +3,7 @@ echo "Downloading few Dependecies . . ."
 git clone --depth=1 https://github.com/Yara0202/Kernel_Asus_X01AD-X X01AD
 git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang clang
 
+
 # Main
 KERNEL_NAME=kernel_Test # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
